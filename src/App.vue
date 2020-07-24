095241471fd3389ce157f34d274165e3bad32646
star <star@starbook.fritz.box>
2020-07-24 20:24:42 +0200

    returning data objects we are using

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
     <div class="container">
 
-      
     </div>
 </template>
 
 <script>
     export default {
-
+        data: function() {
+            return {
+                quotes: [
+                    'This is an example Quote.'
+                ],
+                maxQuotes: 10 // track the max number of Quotes
+            }
+        }
     }
 </script>
 
