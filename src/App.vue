a21989cf1677277e94faf289ce891e09a473b1c4
star <star@starbook.fritz.box>
2020-07-25 12:20:35 +0200

    custom events, quotes added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="container">
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
         <app-quote-grid :quotes="quotes"></app-quote-grid>     <!-- add property :quote="quotes" (v-bind) expecting props in QuoteGrid.vue passing array in it -->
     </div>
 </template>
 
 <script>
     import QuoteGrid from './components/QuoteGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
 
     export default {
         data: function() {
@@ -13,11 +15,17 @@
                 quotes: [
                     'This is an example Quote.'
                 ],
-                maxQuotes: 10 // track the max number of Quotes
+                maxQuotes: 10                       // track the max number of Quotes
+            }
+        },
+        methods: {
+            newQuote(quote) {                       // fetch or access the quote from the returned Object of NewQuote.vue / the $event
+                this.quotes.push(quote);            // how to use the quote
             }
         },
         components: {
-            appQuoteGrid: QuoteGrid
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote
         }
     }
 </script>
