0de786887676815d5e1cbeffe5aba448cd9ca890
star <star@starbook.fritz.box>
2020-07-25 17:22:23 +0200

    quote deletion

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <app-new-quote @quoteAdded="newQuote"></app-new-quote>
-        <app-quote-grid :quotes="quotes"></app-quote-grid>     <!-- add property :quote="quotes" (v-bind) expecting props in QuoteGrid.vue passing array in it -->
+        <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>     <!-- add property :quote="quotes" (v-bind) expecting props in QuoteGrid.vue passing array in it -->
         <div class="row">
             <div class="col-sm-12 text-center">
                 <div class="alert alert-info">Info: Click on a Quote to Delete it!</div>
@@ -26,6 +26,9 @@
         methods: {
             newQuote(quote) {                       // fetch or access the quote from the returned Object of NewQuote.vue / the $event
                 this.quotes.push(quote);            // how to use the quote
+            },
+            deleteQuote(index) {
+                this.quotes.splice(index, 1);       // access quotes Array, start splicing at position of index, then splice one element / modify existing array and removing one element
             }
         },
         components: {
--- src/components/QuoteGrid.vue
+++ src/components/QuoteGrid.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="row">
-        <app-quote v-for="quote in quotes" :key="quote.id">{{ quote }}</app-quote>
+        <app-quote v-for="(quote, index) in quotes" :key="quote.id" @click.native="deleteQuote(index)">{{ quote }}</app-quote>      <!--  .native modifier (deletion of quote) -->
     </div>
 </template>
 
@@ -11,6 +11,11 @@
         props: ['quotes'],
         components: {                   // register the single quote
             appQuote: Quote             // assign appQuote as a Selector, then use it in template "<appQuote></appQuote>""
+        },
+        methods: {
+            deleteQuote(index) {             // add the second Argument "current index" in v-for, pass index in @click deleteQuote(index), and in method.
+                this.$emit('quoteDeleted', index);
+            }
         }
     }
 </script>
