5e2f7e8127615dd26712c27924df4e1b6f76d4f5
star <star@starbook.fritz.box>
2020-07-25 07:27:15 +0200

    create quotes with newquote components

---
+++ src/components/NewQuote.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="row">
+        <form>
+            <div class="col-sm-8 col-sm-offset-2 col-sx-12 col-md-6 col-md-offset-3 form-group">
+                <label>Quote</label>
+                <textarea class="form-control" rows="3" v-model="quote"></textarea>
+            </div>
+            <div class="col-sm-8 col-sm-offset-2 col-sx-12 col-md-6 col-md-offset-3 form-group">
+                <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>   <!-- .prevent = don't submit the form, don't send request to the server to reload the page / prevent the default behavior -->
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                quote: ''        // binding with the textarea / gets set when we enter something in textarea
+            };
+        },
+        methods: {
+            createNew() {
+                
+            }
+        }
+    }
+</script>
+
+<style>
+    
+</style>
